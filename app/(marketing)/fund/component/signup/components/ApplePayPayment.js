58675a0f275c015c050c5103ce6975eec311010e
kajal <>
2024-08-30 16:10:33 +0530

    Change design

--- app/(marketing)/fund/component/signup/components/ApplePayPayment.js
+++ app/(marketing)/fund/component/signup/components/ApplePayPayment.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { PaymentRequestButtonElement, useStripe, useElements } from '@stripe/react-stripe-js';
-// import StatusMessages, { useMessages } from './StatusMessages';
-import { toast } from 'react-toastify';
 
 const ApplePayPayment = ({ amount }) => {
     const stripe = useStripe();
     const elements = useElements();
     const [paymentRequest, setPaymentRequest] = useState(null);
-    // const [messages, addMessage] = useMessages();
 
     useEffect(() => {
         if (!stripe || !elements) {
@@ -18,7 +15,7 @@ const ApplePayPayment = ({ amount }) => {
             country: 'IN',
             currency: 'usd',
             total: {
-                label: 'Demo total',
+                label: 'Total',
                 amount: amount ? amount : 100,
             },
             requestPayerName: true,
@@ -32,57 +29,23 @@ const ApplePayPayment = ({ amount }) => {
             }
         });
 
-        // pr.on('paymentmethod', async (e) => {
-        //     const { error: backendError, clientSecret } = await fetch(
-        //         '/api/create-payment-intent',
-        //         {
-        //             method: 'POST',
-        //             headers: {
-        //                 'Content-Type': 'application/json',
-        //             },
-        //             body: JSON.stringify({
-        //                 paymentMethodType: 'card',
-        //                 currency: 'usd',
-        //             }),
-        //         }
-        //     ).then((r) => r.json());
-
-        //     if (backendError) {
-        //         addMessage(backendError.message);
-        //         return;
-        //     }
-
-        //     toast.error("Client secret returned")
-        //     // addMessage('Client secret returned');
-
-        //     const {
-        //         error: stripeError,
-        //         paymentIntent,
-        //     } = await stripe.confirmCardPayment(clientSecret, {
-        //         payment_method: e.paymentMethod.id,
-        //     }, { handleActions: false });
-
-        //     if (stripeError) {
-        //         // Show error to your customer (e.g., insufficient funds)
-        //         toast.error(stripeError.message)
-        //         // addMessage(stripeError.message);
-        //         return;
-        //     }
-
-        //     // Show a success message to your customer
-        //     // There's a risk of the customer closing the window before callback
-        //     // execution. Set up a webhook or plugin to listen for the
-        //     // payment_intent.succeeded event that handles any business critical
-        //     // post-payment actions.
-        //     toast.success(`Payment ${paymentIntent.status}: ${paymentIntent.id}`)
-        //     // addMessage(`Payment ${paymentIntent.status}: ${paymentIntent.id}`);
-        //     // addMessage(`Payment ${paymentIntent.status}: ${paymentIntent.id}`);
-        // });
     }, [stripe, elements]);
+
+    if (!paymentRequest) {
+        return null;
+    }
 
     return (
         <>
-            {paymentRequest && <PaymentRequestButtonElement options={{ paymentRequest }} />}
+            {paymentRequest && <PaymentRequestButtonElement options={{
+                paymentRequest, style: {
+                    paymentRequestButton: {
+                        type: 'default',
+                        theme: 'light',
+                        // height: '64px',
+                    },
+                },
+            }} />}
         </>
     );
 };
